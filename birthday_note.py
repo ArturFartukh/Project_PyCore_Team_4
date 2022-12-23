e42e89029836dc995efb6a35786e9c2c3f29730f
Hubsit <vitaliy.kievskiy@hotmail.com>
2022-12-23 22:24:57 +0200

    add class Note

--- birthday_note.py
+++ birthday_note.py
@@ -1,10 +1,11 @@
-from datetime import datetime
+from datetime import date, datetime
 import re
+
 
 class Birthday(Field):
 
     @Field.value.setter
-    def value(self, value):
+    def value(self, value: str):
         pattern = r'\d{2}\.\d{2}\.\d{4}'
         result = re.fullmatch(pattern, value)
         if not result:
@@ -12,8 +13,20 @@ class Birthday(Field):
         birthday_date = datetime.strptime(value, '%d.%m.%Y').date()
         if birthday_date > date.today():
             raise ValueError('Birthday must be less than current year and date')
-        self._value = value
+        self._value = birthday_date
 
 
 class Note(Field):
-    pass
+
+    def __init__(self, value):
+        super().__init__(value)
+        self.__tags = None
+
+    @property
+    def tags(self):
+        return self.__tags
+
+    @tags.setter
+    def tags(self, tags: str):
+        new_tags = tags.split(', ')
+        self.__tags = new_tags

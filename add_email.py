0d1da8e17fd1897b9d042cd30da8ff6631927324
SergiiGoncharov13 <goncharov.s.work@gmail.com>
2022-12-24 14:22:25 +0200

    Update add_email.py
    fix

--- add_email.py
+++ add_email.py
@@ -3,13 +3,12 @@ from Project_PyCore_Team_4 import Record, AddressBook
 
 def add_email(data: str):
     name, email = split_data(data)
-    if name not in book:
+    if book.name:
         raise ValueError('This user not in contact book')
-    record = Record(name)
-    for mail in email:
+    if book.mail:
         raise ValueError('Email for this contact already exist')
-    record = add_email(mail)
-
+    record = add_email(email)
+    book.add_record(record)
     return f'Email: {email} has been added to contact: {name}'
 
 

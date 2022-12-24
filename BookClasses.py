1d10d4d3a4a6b72edb36cb1a3e9d1c84c7e62359
Hubsit <vitaliy.kievskiy@hotmail.com>
2022-12-24 22:00:29 +0200

    merge Birthday and Note classes

--- BookClasses.py
+++ BookClasses.py
@@ -226,3 +226,42 @@ class Email(Field):
         if not email:
             raise ValueError('Invalid Email')
         self._value = email
+
+
+class Birthday(Field):
+
+    @Field.value.setter
+    def value(self, value: str):
+        pattern = r'\d{2}\.\d{2}\.\d{4}'
+        result = re.fullmatch(pattern, value)
+        if not result:
+            raise ValueError('Wrong birthday date. Please, input DD.MM.YYYY')
+        birthday_date = datetime.strptime(value, '%d.%m.%Y').date()
+        if birthday_date > date.today():
+            raise ValueError('Birthday must be less than current year and date')
+        self._value = birthday_date
+
+
+class Note(Field):
+
+    def __init__(self, value: str):
+        super().__init__(value)
+        self.__tags = None
+
+    @property
+    def tags(self):
+        return self.__tags
+
+    @tags.setter
+    def tags(self, tags: str):
+        new_tags = []
+        tag = ''
+        for element in tags:
+            if element.isalpha() or element.isnumeric():
+                tag += element
+            elif not element.isalpha() and not element.isnumeric() and tag:
+                new_tags.append(tag)
+                tag = ''
+            elif not element.isalpha() and not element.isnumeric() and not tag:
+                continue
+        self.__tags = new_tags

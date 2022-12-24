c058b297e48143e7059fd981757b4f35dd9c652b
Artur Fartukh <ar4ik.8933@gmail.com>
2022-12-24 13:07:16 +0200

    add phone_validator

---
+++ phone_validator.py
@@ -0,0 +1,16 @@
+def phone_validator(phone: str) -> str:
+    if len(phone) == 13 and phone[0] != '+' and not phone[1:].isdigit():
+        raise ValueError('Wrong phone!')
+    elif len(phone) == 10 and not phone.isdigit():
+        raise ValueError('Wrong phone!')
+    elif not 10 <= len(phone) <= 13:
+        raise ValueError('Wrong phone!')
+    elif len(phone) == 10:
+        phone = '+38' + phone
+    elif len(phone) == 11:
+        phone = '+3' + phone
+    elif len(phone) == 10:
+        phone = '+38' + phone
+    elif len(phone) == 12:
+        phone = '+' + phone
+    return phone

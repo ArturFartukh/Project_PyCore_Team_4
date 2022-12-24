e4de4b82a35122f13c10dae13a221191c46b9a2c
SergiiGoncharov13 <goncharov.s.work@gmail.com>
2022-12-24 13:20:51 +0200

    Create add_email.py

---
+++ add_email.py
@@ -0,0 +1,15 @@
+from Project_PyCore_Team_4 import Record, AddressBook
+
+
+def add_email(data: str):
+    name, email = data.strip().split(' ')
+    if name not in book:
+        raise ValueError('This user not in contact book')
+    record = Record(name)
+    for mail in email:
+        raise ValueError('Email for this contact already exist')
+    record = add_email(mail)
+
+    return f'Email: {email} has been added to contact: {name}'
+
+

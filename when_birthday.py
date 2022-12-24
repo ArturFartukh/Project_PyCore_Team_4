b6820d32a44b549064a30cb129fc6d8ea549c430
SergiiGoncharov13 <goncharov.s.work@gmail.com>
2022-12-24 19:29:36 +0200

    add def days_before_birthday

---
+++ when_birthday.py
@@ -0,0 +1,14 @@
+from datetime import datetime, date
+import Birthday
+import Record
+
+
+def days_before_birthday(data: str):
+    name = data.strip().title()
+    if name not in book:
+        raise ValueError('No such contact found!')
+    record = book[name]
+    result = record.days_to_birthday(data)
+    return result
+
+

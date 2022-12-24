fd354174208b115c5d99624e68db78c22f576bce
SergiiGoncharov13 <goncharov.s.work@gmail.com>
2022-12-24 16:34:44 +0200

    fix

--- add_address.py
+++ add_address.py
@@ -2,10 +2,9 @@
 
 def add_address(data: str):
     name, address = split_data(data)
-    if book.name:
+    if name not in book.data.keys():
         raise ValueError('This user not in contact book')
     if book.address:
         raise ValueError('This contact already exist address')
-    record = add_address(address)
-    book.add_record(record)
+    record = book[name]
     return f'Address: {address} has been added to contact: {name}'

37aebe6ea496ca2775a4895b8241811ff8e9c67d
Artur Fartukh <ar4ik.8933@gmail.com>
2022-12-26 01:30:19 +0200

    Create all_numbers_func.py

---
+++ Functions/all_numbers_func.py
@@ -0,0 +1,18 @@
+def all_numbers_func(book) -> str:
+    """Displays the entire phonebook."""
+
+    print('=' * 30)
+    result = ''
+
+    phone_book = book.get_all_contacts()
+
+    for contact in phone_book:
+        for name in contact:
+            result += f'{"-" * 15}\n' + f'{name}:\n'
+            for number in contact[name]:
+                result += '{:<15}|\n'.format(number)
+
+    if result:
+        return result + f'{"=" * 30}\n'
+    else:
+        return 'The book is empty.\n' + "=" * 30

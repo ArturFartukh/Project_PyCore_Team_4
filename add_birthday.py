5268b3a14716f928fd23319a1637bc1d458f0d8b
Artur Fartukh <112070792+ArturFartukh@users.noreply.github.com>
2022-12-25 16:44:49 +0200

    Update add_birthday.py

--- add_birthday.py
+++ add_birthday.py
@@ -2,7 +2,7 @@ def add_birthday(data: str):
     name, birth_date = split_data(data)
     if name in book.data.keys():
         contact = book.data[name]
-        contact.birthday = contact.add_birthday(birth_date)
+        contact.add_birthday(birth_date)
         return f'\nBirthday has been added [{name}]:[{contact.birthday}]\n'
     else:
         raise ValueError('This user not in contact book')

79420d68a23741921f74e7c150d6b56006e0604f
Artur Fartukh <ar4ik.8933@gmail.com>
2022-12-25 22:17:16 +0200

    Update searsh_contact.py

--- Functions/searsh_contact.py
+++ Functions/searsh_contact.py
@@ -1,13 +1,31 @@
 from main import book
+from phone_validator import *
 
 
 def searsh_contact(data: str) -> str:
-    name = data.strip().title()
-    result = ''
-    if name not in book.data.keys():
+
+    name = None
+    phone = None
+    contact = None
+
+    if data[0].isalpha():
+        name = data.strip().lower()
+    elif data[0].isdigit() or (data[0] == '+' and data[1:].isdigit()):
+        phone = phone_validator(data)
+
+    if name and name not in book.data.keys():
         return f'Contact with the name [{name}] not found.'
-    contact = book[name]
+    elif name:
+        contact = book[name]
+    elif phone:
+        for record in book.data:
+            if record.phone_in_contact(phone):
+                contact = record
+    else:
+        return 'Nothing found.\n'
+
     contact_info = contact.get_all_info()
+    result = ''
     for key in contact_info:
         result += f'{key}: '
         if isinstance(contact_info[key], list):
@@ -17,5 +35,5 @@ def searsh_contact(data: str) -> str:
         else:
             result += f'{contact_info[key]}'
         result += '\n'
-
     return result
+

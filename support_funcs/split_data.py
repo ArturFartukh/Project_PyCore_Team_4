0ee54f98612bf3579e66d078d6ab4b93935153a7
Artur Fartukh <ar4ik.8933@gmail.com>
2022-12-27 16:17:11 +0200

    Update split_data.py

--- support_funcs/split_data.py
+++ support_funcs/split_data.py
@@ -4,8 +4,8 @@ from support_funcs import phone_validator
 def split_data(data: str) -> tuple:
     normalize_data = data.strip()
     name = normalize_data.split(' ')[0].title()
-    if name.isnumeric():
-        return 'Wrong name.\n'
+    if not name[0].isalpha():
+        name = ''
     information = ' '.join(normalize_data.split(' ')[1:])
     information = phone_validator(information)
 

fbb3097622bf234f44669abb839bba02afb74e28
Artur Fartukh <ar4ik.8933@gmail.com>
2022-12-27 03:04:23 +0200

    Create __init__.py

---
+++ Classes/__init__.py
@@ -0,0 +1,22 @@
+from Classes.Field import Field
+from Classes.Name import Name
+from Classes.Phone import Phone
+from Classes.Address import Address
+from Classes.Email import Email
+from Classes.Birthday import Birthday
+from Classes.Name import Name
+from Classes.Record import Record
+from Classes.AddressBook import AddressBook
+
+__all__ = ['Field',
+           'Name',
+           'Phone',
+           'Address',
+           'Email',
+           'Birthday',
+           'Note',
+           'Record',
+           'AddressBook'
+           ]
+
+print('Loading Classes...')

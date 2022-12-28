6987a5c641ce9323cecb5dfaa99fc723856feaa0
Artur Fartukh <ar4ik.8933@gmail.com>
2022-12-28 07:25:11 +0200

    Create except_decorator

--- support_funcs/__init__.py
+++ support_funcs/__init__.py
@@ -1,7 +1,8 @@
 from support_funcs.phone_validator import phone_validator
 from support_funcs.split_data import split_data
+from support_funcs.except_decorator import input_error
 
-__all__ = ['phone_validator', 'split_data']
+__all__ = ['phone_validator', 'split_data', 'input_error']
 
 print('Loading support_funcs...')
 

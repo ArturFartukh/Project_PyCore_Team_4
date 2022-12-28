e1467be9b4c98aba707cea0e84c8372f4ab50ec6
Artur Fartukh <ar4ik.8933@gmail.com>
2022-12-29 00:51:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name='dzvina_assist',
     version='0.0.1',
-    description='Personal assist',
+    description='Personal assistant',
     url='https://github.com/ArturFartukh/dzvina_assist',
     author='Go_IT Team-4',
     author_email='ar4ik.8933@gmail.com',
     license='MIT',
     packages=find_packages(),
     entry_points={'console_scripts': ['dzvina=dzvina_assist.main:start']},
-    include_package_data=True,
+    # include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",

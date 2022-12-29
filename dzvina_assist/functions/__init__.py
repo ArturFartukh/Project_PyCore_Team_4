c80856687c55993fdbe745d0652e1c1cb82c45d6
Artur Fartukh <ar4ik.8933@gmail.com>
2022-12-29 11:08:46 +0200

    Search update

--- dzvina_assist/functions/__init__.py
+++ dzvina_assist/functions/__init__.py
@@ -1,14 +1,12 @@
 from dzvina_assist.functions.address_func import add_address_func, change_address_func
-from dzvina_assist.functions.all_contact_info_func import all_contact_info_func
 from dzvina_assist.functions.all_numbers_func import all_numbers_func
 from dzvina_assist.functions.birthday_func import add_birthday_func, days_before_birthday_func
 from dzvina_assist.functions.contact_func import add_func, change_phone_func, del_func
-from dzvina_assist.functions.search_contact_func import search_contact_func
 from dzvina_assist.functions.search_contact_global_func import search_contact_global_func
 from dzvina_assist.functions.email_func import add_email_func, change_email_func
 from dzvina_assist.functions.note_func import add_note_func, change_notes_func, del_note_func, search_in_notes_func
 from dzvina_assist.functions.tags_func import add_tags_func, find_tags_func
-from dzvina_assist.functions.contact_info_func import contact_info_func
+from dzvina_assist.functions.contact_info_func import contact_info_func, all_contact_info_func
 from dzvina_assist.functions.interactive import start
 
 __all__ = ['start',
@@ -28,7 +26,6 @@ __all__ = ['start',
            'change_notes_func',
            'del_note_func',
            'search_in_notes_func',
-           'search_contact_func',
            'search_contact_global_func',
            'add_tags_func',
            'find_tags_func'
